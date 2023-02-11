dd328f580591181885a21c374a5718137bba9111
mayaaaal <m.lerman@interface3.be>
2023-02-11 12:23:10 +0100

    voila

--- app.js
+++ app.js
@@ -30,64 +30,6 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//
-
-const extendTimeoutMiddleware = (req, res, next) => {
-  const space = " ";
-  let isFinished = false;
-  let isDataSent = false;
-
-  // Only extend the timeout for API requests
-  if (!req.url.includes("/api")) {
-    next();
-    return;
-  }
-
-  res.once("finish", () => {
-    isFinished = true;
-  });
-
-  res.once("end", () => {
-    isFinished = true;
-  });
-
-  res.once("close", () => {
-    isFinished = true;
-  });
-
-  res.on("data", (data) => {
-    // Look for something other than our blank space to indicate that real
-    // data is now being sent back to the client.
-    if (data !== space) {
-      isDataSent = true;
-    }
-  });
-
-  const waitAndSend = () => {
-    setTimeout(() => {
-      // If the response hasn't finished and hasn't sent any data back....
-      if (!isFinished && !isDataSent) {
-        // Need to write the status code/headers if they haven't been sent yet.
-        if (!res.headersSent) {
-          res.writeHead(202);
-        }
-
-        res.write(space);
-
-        // Wait another 15 seconds
-        waitAndSend();
-      }
-    }, 15000);
-  };
-
-  waitAndSend();
-  next();
-};
-
-app.use(extendTimeoutMiddleware);
-
-//
-
 app.get("/", (request, response, next) => {
   response.json({ message: "Hey! This is your server resp!" });
   next();
@@ -130,20 +72,6 @@ app.post("/register", (request, response) => {
         e,
       });
     });
-});
-
-app.get("/db", (request, response) => {
-  dbConnect();
-  User.find({}, function (err, users) {
-    var userMap = {};
-    users.forEach(function (user) {
-      userMap[user._id] = user;
-    });
-    response.send(userMap);
-  });
-});
-app.get("/dburl", (req, res) => {
-  res.send(process.env.DB_URL);
 });
 
 // login endpoint
