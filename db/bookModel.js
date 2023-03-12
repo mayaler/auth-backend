9caaf173bfb1fa84698e66bafc49eaecc31e5b39
mayaler <lemayalerman@gmail.com>
2023-03-12 16:11:01 +0100

    booksModel Change

--- db/bookModel.js
+++ db/bookModel.js
@@ -2,14 +2,17 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const BookSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: Schema.Types.ObjectId, ref: "Author", required: true },
-  summary: { type: String, required: true },
-  isbn: { type: String, required: true },
-  genre: [{ type: Schema.Types.ObjectId, ref: "Genre" }],
-  image: { type: String },
-});
+const BookSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    author: { type: Schema.Types.ObjectId, ref: "Author", required: true },
+    summary: { type: String, required: true },
+    isbn: { type: String, required: true },
+    genre: [{ type: Schema.Types.ObjectId, ref: "Genre" }],
+    image: { type: String },
+  },
+  { collction: "books" }
+);
 
 // Virtual for book's URL
 BookSchema.virtual("url").get(function () {
