3dde420393562faf4974495571db3b9a02d0b8a5
mayaler <lemayalerman@gmail.com>
2023-03-12 16:27:53 +0100

    authorsModel changes

--- db/authorModel.js
+++ db/authorModel.js
@@ -2,12 +2,15 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const AuthorSchema = new Schema({
-  first_name: { type: String, required: true, maxLength: 100 },
-  family_name: { type: String, required: true, maxLength: 100 },
-  date_of_birth: { type: Date },
-  date_of_death: { type: Date },
-});
+const AuthorSchema = new Schema(
+  {
+    first_name: { type: String, required: true, maxLength: 100 },
+    family_name: { type: String, required: true, maxLength: 100 },
+    date_of_birth: { type: Date },
+    date_of_death: { type: Date },
+  },
+  { collction: "authors" }
+);
 
 // Virtual for author's full name
 AuthorSchema.virtual("name").get(function () {
