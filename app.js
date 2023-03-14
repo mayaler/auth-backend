5c19e0d0cdb36e499f417a863ef63ebe598ccf7a
mayaler <lemayalerman@gmail.com>
2023-03-14 16:16:24 +0100

    res.stHeaders

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ dbConnect();
 // Curb Cores Error by adding a header here
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
